985c6d6ce1926395838a81738733ce5ccf646b8b
Fernando Rosa <f.rosaoliveira@gmail.com>
2025-07-04 14:14:52 -0300

    Implementação dos métodos: Editar, Inativar e Buscar por id os Clubes, refatoração do ClubeService e criação de status personalizado NotFoundException

--- src/main/java/br/com/meli/futebolapi/controller/ClubeController.java
+++ src/main/java/br/com/meli/futebolapi/controller/ClubeController.java
@@ -2,14 +2,13 @@ package br.com.meli.futebolapi.controller;
 
 import br.com.meli.futebolapi.dto.ClubeRequestDto;
 import br.com.meli.futebolapi.dto.ClubeResponseDto;
+import br.com.meli.futebolapi.exception.NotFoundException;
 import br.com.meli.futebolapi.service.ClubeService;
 import jakarta.validation.Valid;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -23,15 +22,52 @@ public class ClubeController {
 
     @PostMapping
     public ResponseEntity<?> criarClube(@Valid @RequestBody ClubeRequestDto clubeRequestDto) {
-    try {
-        ClubeResponseDto novo = clubeService.salvar(clubeRequestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(novo);
-     } catch (Exception e) {
-        if (e.getMessage().equals("Clube já existe"))
+        try {
+            ClubeResponseDto novo = clubeService.salvar(clubeRequestDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(novo);
+        } catch (Exception e) {
+            if (e.getMessage().equals("Clube já existe"))
                 return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
-        return ResponseEntity.badRequest().body(e.getMessage());
-     }
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
 
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editarClube(@PathVariable Long id, @Valid @RequestBody ClubeRequestDto clubeRequestDto) {
+        try {
+            ClubeResponseDto atualizado = clubeService.editar(id, clubeRequestDto);
+            return ResponseEntity.status(HttpStatus.OK).body(atualizado);
+        } catch (br.com.meli.futebolapi.exception.NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (org.springframework.dao.DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> inativarClube(@PathVariable Long id) {
+        try {
+            clubeService.inativarClube(id);
+            return ResponseEntity.noContent().build();
+        } catch (br.com.meli.futebolapi.exception.NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
+        try {
+            ClubeResponseDto clube = clubeService.buscarPorId(id);
+            return ResponseEntity.status(HttpStatus.OK).body(clube);
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
 }
+
+
+
--- src/main/java/br/com/meli/futebolapi/service/ClubeService.java
+++ src/main/java/br/com/meli/futebolapi/service/ClubeService.java
@@ -4,6 +4,10 @@ import br.com.meli.futebolapi.model.Clube;
 import br.com.meli.futebolapi.dto.ClubeRequestDto;
 import br.com.meli.futebolapi.dto.ClubeResponseDto;
 import br.com.meli.futebolapi.repository.ClubeRepository;
+// import br.com.meli.futebolapi.repository.PartidaRepository;
+import br.com.meli.futebolapi.exception.NotFoundException;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 
@@ -12,26 +16,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class ClubeService {
 
-    private ClubeRepository clubeRepository;
+    private final ClubeRepository clubeRepository;
 
-    public ClubeService(ClubeRepository clubeRepository) {
-        this.clubeRepository = clubeRepository;
-    }
-
-    public ClubeResponseDto salvar(ClubeRequestDto clubeRequestDto) throws Exception {
-        if(clubeRepository.existsByNomeAndEstado(clubeRequestDto.getNome(), clubeRequestDto.getEstado())) {
-            throw new Exception("Clube já existe");
-        }
-
+    //private final PartidaRepository partidaRepository;
+    private Clube fromRequestDto(ClubeRequestDto clubeRequestDto) {
         Clube clube = new Clube();
         clube.setNome(clubeRequestDto.getNome());
         clube.setEstado(clubeRequestDto.getEstado());
         clube.setDataCriacao(clubeRequestDto.getDataCriacao());
         clube.setStatus(clubeRequestDto.getStatus());
 
-        clube = clubeRepository.save(clube);
+        return clube;
+    }
 
+    private ClubeResponseDto toClubeResponseDto(Clube clube) {
         ClubeResponseDto clubeResponseDto = new ClubeResponseDto();
+
         clubeResponseDto.setId(clube.getId());
         clubeResponseDto.setNome(clube.getNome());
         clubeResponseDto.setEstado(clube.getEstado());
@@ -39,5 +39,61 @@ public class ClubeService {
         clubeResponseDto.setStatus(clube.getStatus());
 
         return clubeResponseDto;
+    }
+
+    public ClubeService(ClubeRepository clubeRepository /*, PartidaRepository partidaRepository */) {
+        this.clubeRepository = clubeRepository;
+        // this.partidaRepository = partidaRepository;
+    }
+
+    public ClubeResponseDto salvar(@NotNull ClubeRequestDto clubeRequestDto) {
+        if (clubeRepository.existsByNomeAndEstado(clubeRequestDto.getNome(), clubeRequestDto.getEstado())) {
+            throw new DataIntegrityViolationException("Clube já existe");
+        }
+
+
+        Clube clube = fromRequestDto(clubeRequestDto);
+        clube = clubeRepository.save(clube);
+
+        return toClubeResponseDto(clube);
+    }
+
+    public ClubeResponseDto editar(Long id, @NotNull ClubeRequestDto clubeRequestDto)  {
+        Clube clube = clubeRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Clube não encontrado"));
+
+        boolean doisClubesMesmoNomeEstado =
+                clubeRepository.existsByNomeAndEstado(clubeRequestDto.getNome(), clubeRequestDto.getEstado())
+                        && !(clube.getNome().equals(clubeRequestDto.getNome())) && clube.getEstado().equals(clubeRequestDto.getEstado());
+        if(doisClubesMesmoNomeEstado) {
+            throw new DataIntegrityViolationException("Já existe um clube com este nome");
+        }
+
+
+        // if (partidaRepository.existsByClubeAndDataAfter(clube, dto.getDataCriacao())) {
+        //     throw new DataIntegrityViolationException("Não pode definir data de criação posterior a uma partida já realizada por esse clube");
+        // }
+
+        fromRequestDto(clubeRequestDto);
+
+        clube = clubeRepository.save(clube);
+
+        return toClubeResponseDto(clube);
+
+    }
+
+    public void inativarClube(Long id) {
+        Clube clube = clubeRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Clube não encontrado"));
+        clube.setStatus(false);
+
+        clubeRepository.save(clube);
+    }
+
+    public ClubeResponseDto buscarPorId(Long id) {
+        Clube clube = clubeRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Clube não encontrado"));
+
+        return toClubeResponseDto(clube);
     }
 }
